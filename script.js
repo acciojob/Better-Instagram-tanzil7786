2f1c3e6a2b58c5aa16a52e91f3ed8dcdbad63f87
Tanzil Pasha <84717048+tanzil7786@users.noreply.github.com>
2023-07-23 22:30:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,31 +1,37 @@
 //your code here
-const parent=document.getElementById("parent");
-let setImage=null;
 
-parent.addEventListener("dragstart" , (e) =>{
-	setImage=e.target;
+const parent = document.getElementById("parent");
+let selectedImage = null;
+
+// Add event listeners for drag events
+parent.addEventListener("dragstart", (e) => {
+  selectedImage = e.target;
 });
 
-parent.addEventListener("dragover"  ,(e)=>{
-	e.preventDefault();
-});
-parent.addEventListener("drop" ,(e) =>{
-	e.preventDefault();
-	if(setImage !== null){
-		swapImage(setImage , e.target);
-		setImage=null;
-	}
+parent.addEventListener("dragover", (e) => {
+  e.preventDefault();
 });
 
-function swapImage(image1,image2){
-	const image1Index = Array.from(parent.children).indexOf(image1);
-	const image1Index = Array.from(parent.children).indexOf(image1);
+parent.addEventListener("drop", (e) => {
+  e.preventDefault();
+  if (selectedImage !== null) {
+    swapImages(selectedImage, e.target);
+    selectedImage = null;
+  }
+});
 
-	const tempBackground = image1.style.backgroundImage;
-	image1.style.backgroundImage = image2.style.backgroundImage;
-	image2.style.backgroundImage = tempBackground;
+// Function to swap images between two divs
+function swapImages(image1, image2) {
+  const image1Index = Array.from(parent.children).indexOf(image1);
+  const image2Index = Array.from(parent.children).indexOf(image2);
 
-	const tempText = image1.textContent;
-	image1.textContent=image1.textContent;
-	image2.textContent=tempText;
+  // Swap the background images
+  const tempBackground = image1.style.backgroundImage;
+  image1.style.backgroundImage = image2.style.backgroundImage;
+  image2.style.backgroundImage = tempBackground;
+
+  // Swap the text content
+  const tempText = image1.textContent;
+  image1.textContent = image2.textContent;
+  image2.textContent = tempText;
 }
